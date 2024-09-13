9aa4589351643e94117ccdf87ba5a93a643d1173
Martin Sanchez <tinchofiuba@gmail.com>
2024-09-13 19:13:17 -0300

    me comunique por serial, igual no pude con el attiny! solo con nano

--- serialPython.py
+++ serialPython.py
@@ -45,24 +45,44 @@ while True:
 
 import serial
 import time
-#todo lo que esta abajo lo repito indefinidamente
 
-while True:
-    ser = serial.Serial('/dev/ttyUSB0', 9600, timeout=1)
+def main():
+    # Configurar el puerto serial (reemplaza '/dev/ttyUSB0' con el puerto correcto)
+    ser = serial.Serial('/dev/ttyS4', 9600, timeout=0.5)
+
     # Esperar un momento para asegurarse de que la conexión esté establecida
-    time.sleep(2)
+    time.sleep(0.3)
+    print("INGRESAR ORDENES PARA EL MOTOR:")
+    print("-------------------------------")
+    print("PRIMERO SE ENVIA EL DELAY DE ENERGIZACION DEL MOTOR, LUEGO LA DIRECCION DE GIRO Y FINALMENTE EL ESTADO--->"+"1000,1,1")
+    print("--LA VELOCIDAD DEBE AUMENTA AL DISMINUIR EL VALOR DEL DELAY (MAYOR A 800 SIEMPRE)")
+    print("--GIRA EN SENTIDO HORARIO SI EL VALOR ES 1 Y ANTIHORARIO SI ES 0")
+    print("--EL ESTADO DEBE SER 1 PARA ENCENDER EL MOTOR Y 0 PARA APAGARLO")
+    while True:
+        # Pedir al usuario que ingrese un array
+        array_to_send = input("Ingresar array de ordenes (para enviar al Arduino (por ejemplo, '1000,1000,0,1') o 'exit' para salir: ")
+        if array_to_send.lower() == 'exit':
+            break
 
-    # Enviar un mensaje al Arduino
-    ser.write(b'Hello ')
-    time.sleep(5)
-    ser.write(b'Hello Arduino')
-    if ser.in_waiting > 0:
-        try:
-            response = ser.readline().decode('utf-8').strip()
-            print("okokok")
-        except UnicodeDecodeError:
-            response = ser.readline().decode('latin-1').strip()
-            print("hay algo raro")
-        print(f"Respuesta del Arduino: {response}")
+        # Enviar el array al Arduino
+        ser.write(array_to_send.encode())
+        time.sleep(1)  # Esperar un segundo antes de leer la respuesta
+
+        # Leer la respuesta del Arduino
+        if ser.in_waiting > 0:
+            try:
+                response = ser.readline().decode('utf-8').strip()
+                print("Se decodifico según utf-8")
+            except UnicodeDecodeError:
+                response = ser.readline().decode('latin-1').strip()
+                print("Se decodifico según latin-1")
+            print(f"Respuesta del Arduino: {response}")
+
+        # Esperar un momento antes de enviar el siguiente array
+        time.sleep(0.3)
+
     # Cerrar el puerto serial
-ser.close()
+    ser.close()
+
+if __name__ == "__main__":
+    main()
